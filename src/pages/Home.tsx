0d381b3039dbfe9a297daa1e93a376962bdf9e30
md-sazzadul <md.sazzadul.islam15@gmail.com>
2026-04-03 13:25:51 +0200

    connect everything

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,22 @@
-import MainLayout from "../layouts/MainLayout";
+import Navbar from '../components/Navbar';
+import MainLayout from '../layouts/MainLayout';
+import About from '../sections/About';
+import Hero from '../sections/Hero';
+import Projects from '../sections/Projects';
+import Skills from '../sections/Skills';
 
 const Home = () => {
   return (
-    <MainLayout>
-      <h1 className="text-3xl">Portfolio</h1>
-    </MainLayout>
+    <>
+      <Navbar />
+
+      <MainLayout>
+        <Hero />
+        <About />
+        <Skills />
+        <Projects />
+      </MainLayout>
+    </>
   );
 };
 
