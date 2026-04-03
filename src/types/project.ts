394fb229c37134c3e2fe0fb64b44ee12680c3c87
md-sazzadul <md.sazzadul.islam15@gmail.com>
2026-04-03 12:52:40 +0200

    define types

---
+++ src/types/project.ts
@@ -0,0 +1,10 @@
+export interface Project {
+  title: string;
+  description: string;
+  problem: string;
+  solution: string;
+  impact: string;
+  tech: string[];
+  github: string;
+  live: string;
+}
